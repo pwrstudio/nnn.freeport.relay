517531b5ee5bb548fc71b8066da34d878c714ac1
pwrstudio <pwrpntr@gmail.com>
2017-12-17 21:23:07 +0100

    basic geolocation

--- app.js
+++ app.js
@@ -1,19 +1,50 @@
-const express = require('express')
-const app = express()
-const port = 80
-const server = app.listen(port)
-const io = require('socket.io').listen(server)
+const PORT = process.env.PORT || 9090
+const server = require('http').createServer()
+const io = require('socket.io')(server)
+var geoip = require('geoip-lite')
+server.listen(PORT)
 
 let allClients = []
 
 io.sockets.on('connection', socket => {
-  allClients.push(socket.id)
-  io.emit('enter', {user: socket.handshake.headers.origin, list: allClients})
+  // TODO: validate client ip
+  // console.log(socket.conn.request.headers.referer)
+  // console.log(socket.handshake.headers.origin)
+  // console.log(socket.handshake)
+  // console.log(socket.handshake.headers['x-real-ip'])
+  // console.log(socket.handshake.headers['x-forwarded-for'])
+  // console.log(socket.request.socket.remoteAddress)
+
+  let clientIp = socket.handshake.headers['x-forwarded-for']
+  let clientGeo = geoip.lookup(clientIp)
+
+  if (!clientGeo) {
+    clientGeo = {
+      range: null,
+      country: null,
+      region: null,
+      city: null,
+      ll: null,
+      metro: null,
+      zip: null
+    }
+  }
+
+  console.log(clientGeo)
+
+  let user = {
+    ip: clientIp,
+    id: socket.id,
+    geo: clientGeo
+  }
+  allClients.push(user)
+
+  io.emit('enter', {user: user, list: allClients})
 
   socket.on('disconnect', () => {
     let i = allClients.indexOf(socket.id)
     allClients.splice(i, 1)
-    io.emit('leave', {user: socket.handshake.headers.origin, list: allClients})
+    io.emit('leave', {user: socket.handshake.headers['x-forwarded-for'], list: allClients})
   })
 
   socket.on('view', data => {
